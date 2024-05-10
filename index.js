939173c759e7f0e717da7eaf503a25b76893e496
chideradev <chideraonyekachi04@gmail.com>
2024-05-10 12:45:58 +0300

    Updates

---
+++ index.js
@@ -0,0 +1,25 @@
+const image = document.getElementById('responsive-image');
+
+// Function to update the image source based on viewport size
+function updateImageSrc() {
+  const viewportWidth = window.innerWidth;
+
+ if (viewportWidth <= 920) {
+    image.src = '../assets/images/image-web-3-mobile.jpg';
+    console.log("hey")
+  } else {
+    image.src = '../assets/images/image-web-3-desktop.jpg';
+  }
+}
+updateImageSrc();
+
+// Add event listener to update the image source when the window is resized
+window.addEventListener('resize', updateImageSrc);
+// toggle hamburger
+const menuIcon = document.getElementById('menu-icon');
+const navLinks = document.getElementById('nav-ul-big');
+
+menuIcon.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+  menuIcon.classList.toggle('active');
+});
